49eb2782e3f320b8e94488d833388827a9068133
zhanghuaimin666 <3416214532@qq.com>
2026-07-21 22:02:56 +0800

    Add static version for GitHub Pages (gh-pages/ directory)

---
+++ gh-pages/sw.js
@@ -0,0 +1,41 @@
+﻿var CACHE_NAME = "pomodoro-static-v1";
+var ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./task-link.css",
+  "./theme-variants.css",
+  "./timer.js",
+  "./manifest.json",
+  "./icon.svg"
+];
+
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(ASSETS);
+    }).then(function() {
+      return self.skipWaiting();
+    })
+  );
+});
+
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(cached) {
+      return cached || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("activate", function(event) {
+  event.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(k) { return k !== CACHE_NAME; }).map(function(k) { return caches.delete(k); })
+      );
+    }).then(function() {
+      return self.clients.claim();
+    })
+  );
+});
